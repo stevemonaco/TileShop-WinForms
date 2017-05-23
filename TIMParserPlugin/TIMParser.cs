ec6d81a17d8c378e463e43614a8b57e869a4906c
Steve <stevemonaco@hotmail.com>
2017-05-22 23:48:21 -0400

    Returns files through plugin contract instead of FileManager

--- TIMParserPlugin/TIMParser.cs
+++ TIMParserPlugin/TIMParser.cs
@@ -23,6 +23,7 @@ namespace TIMParserPlugin
     {
         List<Arranger> arrangers = new List<Arranger>();
         List<Palette> palettes = new List<Palette>();
+        List<string> filenames = new List<string>();
 
         const uint TimTag = 0x00000010;
 
@@ -45,7 +46,11 @@ namespace TIMParserPlugin
             palettes.Clear();
 
             foreach (string fname in ofd.FileNames)
-                SearchTIMFile(fname);
+            {
+                int count = SearchTIMFile(fname);
+                if (count > 0) // Found at least one TIM in the file
+                    filenames.Add(fname);
+            }
 
             return true;
         }
@@ -55,100 +60,94 @@ namespace TIMParserPlugin
             if (!File.Exists(FileName))
                 throw new FileNotFoundException();
 
-            bool FileAdded = false;
             int TimCount = 0;
 
             try
             {
-                FileStream fs = new FileStream(FileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, 65536, FileOptions.SequentialScan);
-                BinaryReader br = new BinaryReader(fs);
-
-                for(int i = 0; i < fs.Length / 4; i++)
+                using (FileStream fs = new FileStream(FileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, 65536, FileOptions.SequentialScan))
                 {
-                    if (fs.Position + 8 > fs.Length) // Ensure enough bytes remaining for header tags
-                        break;
+                    BinaryReader br = new BinaryReader(fs);
 
-                    if (br.ReadUInt32() != TimTag)
-                        continue;
-
-                    uint ImageTag = br.ReadUInt32();
-
-                    if (ImageTag > 15) // Value too large to be a valid image tag
-                        continue;
-
-                    TimData td = new TimData();
-
-                    // Detect color depth
-                    if ((ImageTag & 0b111) == 0b000)
-                        td.ColorDepth = 4;
-                    else if ((ImageTag & 0b111) == 0b001)
-                        td.ColorDepth = 8;
-                    else if ((ImageTag & 0b111) == 0b010)
-                        td.ColorDepth = 16;
-                    else if ((ImageTag & 0b111) == 0b011)
-                        td.ColorDepth = 24;
-                    else
-                        continue; // Invalid color depth
-
-                    // Read CLUT header if present
-                    if ((ImageTag & 0b1000) == 0b1000) // Clut flag enabled
+                    for (int i = 0; i < fs.Length / 4; i++)
                     {
-                        if (fs.Position + 12 > fs.Length) // Ensure enough bytes remaining for CLUT header
+                        if (fs.Position + 8 > fs.Length) // Ensure enough bytes remaining for header tags
                             break;
 
-                        td.ClutBlockSize = br.ReadInt32() - 12;
-                        td.PaletteOrgX = br.ReadUInt16();
-                        td.PaletteOrgY = br.ReadUInt16();
-                        td.ClutColors = br.ReadUInt16();
-                        td.ClutCount = br.ReadUInt16();
-
-                        int ClutSize = td.ClutColors * 2;
-
-                        if (td.ClutColors * td.ClutCount * 2 != td.ClutBlockSize) // CLUT size verification
+                        if (br.ReadUInt32() != TimTag)
                             continue;
 
-                        for (int j = 0; j < td.ClutCount; j++)
-                            td.ClutOffsets.Add(br.BaseStream.Position + ClutSize * j);
+                        uint ImageTag = br.ReadUInt32();
 
-                        if (fs.Position + td.ClutBlockSize > fs.Length) // Ensure enough bytes remaining for CLUT
+                        if (ImageTag > 15) // Value too large to be a valid image tag
+                            continue;
+
+                        TimData td = new TimData();
+
+                        // Detect color depth
+                        if ((ImageTag & 0b111) == 0b000)
+                            td.ColorDepth = 4;
+                        else if ((ImageTag & 0b111) == 0b001)
+                            td.ColorDepth = 8;
+                        else if ((ImageTag & 0b111) == 0b010)
+                            td.ColorDepth = 16;
+                        else if ((ImageTag & 0b111) == 0b011)
+                            td.ColorDepth = 24;
+                        else
+                            continue; // Invalid color depth
+
+                        // Read CLUT header if present
+                        if ((ImageTag & 0b1000) == 0b1000) // Clut flag enabled
+                        {
+                            if (fs.Position + 12 > fs.Length) // Ensure enough bytes remaining for CLUT header
+                                break;
+
+                            td.ClutBlockSize = br.ReadInt32() - 12;
+                            td.PaletteOrgX = br.ReadUInt16();
+                            td.PaletteOrgY = br.ReadUInt16();
+                            td.ClutColors = br.ReadUInt16();
+                            td.ClutCount = br.ReadUInt16();
+
+                            int ClutSize = td.ClutColors * 2;
+
+                            if (td.ClutColors * td.ClutCount * 2 != td.ClutBlockSize) // CLUT size verification
+                                continue;
+
+                            for (int j = 0; j < td.ClutCount; j++)
+                                td.ClutOffsets.Add(br.BaseStream.Position + ClutSize * j);
+
+                            if (fs.Position + td.ClutBlockSize > fs.Length) // Ensure enough bytes remaining for CLUT
+                                break;
+
+                            fs.Seek(td.ClutBlockSize, SeekOrigin.Current); // Skip reading the CLUT
+                        }
+
+                        if (fs.Position + 12 > fs.Length) // Ensure enough bytes remaining for image header
                             break;
 
-                        fs.Seek(td.ClutBlockSize, SeekOrigin.Current); // Skip reading the CLUT
+                        td.ImageDataSize = br.ReadInt32() - 12;
+                        td.ImageOrgX = br.ReadUInt16();
+                        td.ImageOrgY = br.ReadUInt16();
+                        td.ImageWidth = 16 * br.ReadUInt16() / td.ColorDepth;
+                        td.ImageHeight = br.ReadUInt16();
+
+                        if (td.ImageDataSize != td.ImageWidth * td.ImageHeight * td.ColorDepth / 8)
+                            continue;
+
+                        if (fs.Position + td.ImageDataSize > fs.Length) // Ensure enough bytes remaining for image data
+                            break;
+
+                        td.ImageDataOffset = fs.Position;
+                        fs.Seek(td.ImageDataSize, SeekOrigin.Current);
+
+                        Arrange(td, FileName, String.Format("{0}.{1}", FileName, TimCount));
+                        TimCount++;
                     }
-
-                    if (fs.Position + 12 > fs.Length) // Ensure enough bytes remaining for image header
-                        break;
-
-                    td.ImageDataSize = br.ReadInt32() - 12;
-                    td.ImageOrgX = br.ReadUInt16();
-                    td.ImageOrgY = br.ReadUInt16();
-                    td.ImageWidth = 16 * br.ReadUInt16() / td.ColorDepth;
-                    td.ImageHeight = br.ReadUInt16();
-
-                    if (td.ImageDataSize != td.ImageWidth * td.ImageHeight * td.ColorDepth / 8)
-                        continue;
-
-                    if (fs.Position + td.ImageDataSize > fs.Length) // Ensure enough bytes remaining for image data
-                        break;
-
-                    td.ImageDataOffset = fs.Position;
-                    fs.Seek(td.ImageDataSize, SeekOrigin.Current);
-
-                    if (!FileAdded) // TODO: Modify when FileManager is changed to not have files always opened
-                    {
-                        FileManager.Instance.AddFileStream(FileName, fs);
-                        FileAdded = true;
-                    }
-
-                    Arrange(td, FileName, String.Format("{0}.{1}", FileName, TimCount));
-                    TimCount++;
                 }
             }
             catch(Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
-
 
             return TimCount;
         }
@@ -161,6 +160,11 @@ namespace TIMParserPlugin
         public List<Palette> RetrievePalettes()
         {
             return palettes;
+        }
+
+        public List<string> RetrieveFiles()
+        {
+            return filenames;
         }
 
         void Arrange(TimData td, string TimFileName, string BaseName)
