2dd4ff254cb4cc2451daba1fa25f692446fe544a
Steve <stevemonaco@hotmail.com>
2017-07-14 19:40:59 -0400

    Refactored project-specific nodes into new source file

---
+++ TileShop/EditorForms/ProjectTreeNode.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TileShop
+{
+    public abstract class ProjectTreeNode : TreeNode
+    {
+        public abstract void BuildContextMenu(ContextMenu Menu);
+
+        /// <summary>
+        /// Path to folder where the node is contained
+        /// </summary>
+        /*protected string folderPath;
+
+        public string FolderPath
+        {
+            get { return folderPath; }
+            set
+            {
+                folderPath = value;
+                MoveNodeToPath();
+            }
+        }
+
+        private void MoveNodeToPath()
+        {
+            TreeView parentTree = this.TreeView;
+
+
+        }*/
+    }
+
+    public class FolderNode : ProjectTreeNode
+    {
+        public FolderNode()
+        {
+            ImageIndex = 0;
+            SelectedImageIndex = 0;
+        }
+
+        public override void BuildContextMenu(ContextMenu Menu)
+        {
+            Menu.MenuItems.Clear();
+            Menu.MenuItems.Add(new MenuItem("Add New Folder", AddFolder_Click));
+            Menu.MenuItems.Add(new MenuItem("Remove Folder", RemoveFolder_Click));
+        }
+
+        public void AddFolder_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        public void RemoveFolder_Click(object sender, System.EventArgs e)
+        {
+            if (this.Nodes.Count > 0) // This folder contains child nodes that will also need to be removed; warn user
+            {
+                DialogResult dr = MessageBox.Show("Folder contains subitems that will also be removed. Continue?", "Remove Folder", MessageBoxButtons.YesNo);
+                if (dr == DialogResult.No)
+                    return;
+
+                // TODO: Remove subitems
+            }
+            else
+                this.Remove();
+        }
+    }
+
+    public class FileNode : ProjectTreeNode
+    {
+        public FileNode()
+        {
+            ImageIndex = 2;
+            SelectedImageIndex = 2;
+        }
+
+        public override void BuildContextMenu(ContextMenu Menu)
+        {
+            Menu.MenuItems.Clear();
+
+            Menu.MenuItems.Add(new MenuItem("Open File in Viewer", OpenFile_Click));
+            Menu.MenuItems.Add(new MenuItem("Remove File from Project", RemoveFile_Click));
+        }
+
+        public void OpenFile_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        public void RemoveFile_Click(object sender, System.EventArgs e)
+        {
+            this.Remove();
+        }
+    }
+
+    public class PaletteNode : ProjectTreeNode
+    {
+        public PaletteNode()
+        {
+            ImageIndex = 4;
+            SelectedImageIndex = 4;
+        }
+
+        public override void BuildContextMenu(ContextMenu Menu)
+        {
+            Menu.MenuItems.Clear();
+
+            Menu.MenuItems.Add(new MenuItem("Edit Palette", EditPalette_Click));
+            Menu.MenuItems.Add(new MenuItem("Remove Palette from Project", RemovePalette_Click));
+        }
+
+        public void EditPalette_Click(object sender, System.EventArgs e)
+        {
+            //PaletteEditor pe = new PaletteEditor("CharMapPal1");
+
+
+        }
+
+        public void RemovePalette_Click(object sender, System.EventArgs e)
+        {
+
+            this.Remove();
+        }
+    }
+
+    public class ArrangerNode : ProjectTreeNode
+    {
+        public ArrangerNode()
+        {
+            ImageIndex = 3;
+            SelectedImageIndex = 3;
+        }
+
+        public override void BuildContextMenu(ContextMenu Menu)
+        {
+            Menu.MenuItems.Clear();
+
+            Menu.MenuItems.Add(new MenuItem("Open Arranger", OpenArranger_Click));
+            Menu.MenuItems.Add(new MenuItem("Resize Arranger", ResizeArranger_Click));
+            Menu.MenuItems.Add(new MenuItem("Export Arranger", ExportArranger_Click));
+            Menu.MenuItems.Add("-");
+            Menu.MenuItems.Add(new MenuItem("Remove Arranger", RemoveArranger_Click));
+        }
+
+        public void OpenArranger_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        public void ResizeArranger_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        public void ExportArranger_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        public void RemoveArranger_Click(object sender, System.EventArgs e)
+        {
+            this.Remove();
+        }
+    }
+}
