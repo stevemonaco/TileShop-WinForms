a50b48ce5bb879f3c8ac422984d2832f42b5a7b4
Steve <stevemonaco@hotmail.com>
2018-03-23 06:10:54 -0400

    Sorter for project resource tree nodes

---
+++ TileShop/UI/ResourceNodeSorter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+
+namespace TileShop
+{
+    class ResourceNodeSorter : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            ResourceNode rx = x as ResourceNode;
+            ResourceNode ry = y as ResourceNode;
+
+            if (rx is FolderNode && ry is FolderNode)
+                return string.Compare(rx.Text, ry.Text);
+            else if (rx is FolderNode)
+                return -1;
+            else if (ry is FolderNode)
+                return 1;
+            else
+                return string.Compare(rx.Text, ry.Text);
+        }
+    }
+}
