5c2a3c09aca07af81186851283588368a624b6a1
Steve <stevemonaco@hotmail.com>
2018-03-26 23:42:56 -0400

    Renamed from IResource

---
+++ TileShop/Core/IProjectResource.cs
@@ -0,0 +1,19 @@
+﻿namespace TileShop.Core
+{
+    /// <summary>
+    /// Interface specifying how TileShop Project Resource objects must be implemented
+    /// </summary>
+    public interface IProjectResource
+    {
+        /// <summary>
+        /// Identifying name of the resource
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Rename a resource with a new name
+        /// </summary>
+        /// <param name="name"></param>
+        void Rename(string name);
+    }
+}
